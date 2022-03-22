cb9f7d1f1b376f8cfb9805030572f24b396d31b3
fxpw <fxpw@outlook.com>
2022-03-22 21:04:16 +0300

    add ru loc

---
+++ TellMeWhen/Localization/TellMeWhen-ruRU.lua
@@ -0,0 +1,73 @@
+﻿
+if GetLocale() ~= "ruRU" then return end
+TELLMEWHEN_CMD_RESET = "Восстановить";
+TELLMEWHEN_CMD_OPTIONS = "Опции";
+
+TELLMEWHEN_ICON_TOOLTIP1 = "TMW";
+TELLMEWHEN_ICON_TOOLTIP2 = "Щелкните правой кнопкой мыши для выбора параметров значка. Дополнительные параметры в меню параметров интерфейса Blizzard. Введите / tellmewhen или /tmw, чтобы заблокировать и включить надстройку";
+
+TELLMEWHEN_RESIZE = "Изменить размер";
+TELLMEWHEN_RESIZE_TOOLTIP = "Щелкните и перетащите, чтобы изменить размер";
+
+TELLMEWHEN_ICONMENU_CHOOSENAME = "Выберите заклинание / предмет / бафф / и т.д.";
+TELLMEWHEN_CHOOSENAME_DIALOG = "Введите имя или идентификатор заклинания / способности / предмета / баффа / дебаффа, за которым этот значок должен следить. Вы можете добавить несколько баффов / дебаффов, разделив их с помощью ;";
+TELLMEWHEN_ICONMENU_ENABLE = "Включить значек";
+
+TELLMEWHEN_ICONMENU_TYPE = "Тип значка";
+TELLMEWHEN_ICONMENU_COOLDOWN = "Кулдаун";
+TELLMEWHEN_ICONMENU_BUFFDEBUFF = "Баф/Дебаф";
+TELLMEWHEN_ICONMENU_REACTIVE = "Реагирующие спелл или способность";
+TELLMEWHEN_ICONMENU_WPNENCHANT = "Временные чары для оружия";
+
+TELLMEWHEN_ICONMENU_OPTIONS = "Больше настроек";
+
+TELLMEWHEN_ICONMENU_COOLDOWNTYPE = "Тип кулдауна";
+TELLMEWHEN_ICONMENU_SPELL = "Спелл или способность";
+TELLMEWHEN_ICONMENU_ITEM = "Предмет";
+TELLMEWHEN_ICONMENU_SHOWWHEN = "Показывать иконку когда";
+TELLMEWHEN_ICONMENU_USABLE = "Юзабельно";
+TELLMEWHEN_ICONMENU_UNUSABLE = "Неюзабельно";
+
+TELLMEWHEN_ICONMENU_BUFFTYPE = "Бафф или дебаф?";
+TELLMEWHEN_ICONMENU_BUFF = "Бафф";
+TELLMEWHEN_ICONMENU_DEBUFF = "Дебаф";
+TELLMEWHEN_ICONMENU_UNIT = "За кем следить";
+TELLMEWHEN_ICONMENU_PLAYER = "Игрок";
+TELLMEWHEN_ICONMENU_TARGET = "Цель";
+TELLMEWHEN_ICONMENU_FOCUS = "Фокус";
+TELLMEWHEN_ICONMENU_PET = "Питомец";
+TELLMEWHEN_ICONMENU_TARGETTARGET = "ТаргетТаргета";
+TELLMEWHEN_ICONMENU_FOCUSTARGET = "ТаргетФокуса";
+TELLMEWHEN_ICONMENU_PETTARGET = "ТаргетПитомца";
+TELLMEWHEN_ICONMENU_BUFFSHOWWHEN = "Показывать  бафф/дебафф когда ";
+TELLMEWHEN_ICONMENU_PRESENT = "Когда наложен";
+TELLMEWHEN_ICONMENU_ABSENT = "Когда нету";
+TELLMEWHEN_ICONMENU_ALWAYS = "Всегда";
+TELLMEWHEN_ICONMENU_ONLYMINE = "Показывать только если скастовано мною";
+TELLMEWHEN_ICONMENU_SHOWTIMER = "Показывать таймер";
+
+TELLMEWHEN_ICONMENU_WPNENCHANTTYPE = "Слот для оружия для наблюдения";
+TELLMEWHEN_ICONMENU_MAINHAND = "Основная рука";
+TELLMEWHEN_ICONMENU_OFFHAND = "Левая рука";
+--	TELLMEWHEN_ICONMENU_WPNSHOWWHEN = "Show when enchant";
+
+TELLMEWHEN_ICONMENU_CLEAR = "Очистить настройки";
+
+TELLMEWHEN_UIPANEL_SUBTEXT1 = "Эти параметры позволяют изменять количество, расположение и поведение значков напоминаний.";
+TELLMEWHEN_UIPANEL_SUBTEXT2 = "Иконки работают в заблокированном состоянии. В разблокированном состоянии вы можете перемещать / изменять размер группы значков и щелкать правой кнопкой мыши отдельные значки для получения дополнительных настроек. Вы также можете ввести / tellmewhen или / tmw, чтобы заблокировать / разблокировать.";
+TELLMEWHEN_UIPANEL_ICONGROUP = "Группа значков";
+TELLMEWHEN_UIPANEL_ROWS = "Рядов";
+TELLMEWHEN_UIPANEL_COLUMNS = "Колонок";
+TELLMEWHEN_UIPANEL_ONLYINCOMBAT = "Показывать только в бою";
+TELLMEWHEN_UIPANEL_PRIMARYSPEC = "Первый спек";
+TELLMEWHEN_UIPANEL_SECONDARYSPEC = "Второй спек";
+TELLMEWHEN_UIPANEL_GROUPRESETHEADER = "Сброс позиции";
+TELLMEWHEN_UIPANEL_GROUPRESET = "Сброс";
+TELLMEWHEN_UIPANEL_LOCK = "Заблокировать";
+TELLMEWHEN_UIPANEL_UNLOCK = "Разблокировать";
+TELLMEWHEN_UIPANEL_TOOLTIP_ENABLEGROUP = "Показать и включить эту группу значков";
+TELLMEWHEN_UIPANEL_TOOLTIP_ROWS = "Установите количество строк значков в этой группе";
+TELLMEWHEN_UIPANEL_TOOLTIP_COLUMNS = "Установите количество столбцов значков в этой группе";
+TELLMEWHEN_UIPANEL_TOOLTIP_ONLYINCOMBAT = "Установите этот флажок, чтобы отображать эту группу значков только в бою.";
+TELLMEWHEN_UIPANEL_TOOLTIP_PRIMARYSPEC = "Установите этот флажок, чтобы отображать эту группу значков в основной спецификации";
+TELLMEWHEN_UIPANEL_TOOLTIP_SECONDARYSPEC = "Установите этот флажок, чтобы отображать эту группу значков во вторичной спецификации";
